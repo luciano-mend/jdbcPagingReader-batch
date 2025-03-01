73fdd15b3c6f23c187c51961d651393be5d18e12
Luciano Mendonca <luciano.s.mend@gmail.com>
2025-03-01 17:43:41 -0300

    feat: Criação da configuração de Skip

--- src/main/java/br/luciano/JdbcPagingReader_batch/job/JdbcCursorReaderJobConfig.java
+++ src/main/java/br/luciano/JdbcPagingReader_batch/job/JdbcCursorReaderJobConfig.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 public class JdbcCursorReaderJobConfig {
 
     @Bean
-    public Job jdbcCursorReaderJob(Step jdbcCursorReaderStep, JobRepository jobRepository) {
+    public Job jdbcCursorReaderJob(@Qualifier("skipExceptionStep") Step jdbcCursorReaderStep, JobRepository jobRepository) {
         return new JobBuilder("jdbcCursorReaderJob", jobRepository)
                 .start(jdbcCursorReaderStep)
                 .incrementer(new RunIdIncrementer())
--- src/main/java/br/luciano/JdbcPagingReader_batch/step/SkipExceptionStepConfig.java
+++ src/main/java/br/luciano/JdbcPagingReader_batch/step/SkipExceptionStepConfig.java
@@ -7,24 +7,24 @@ import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-public class JdbcCursorReaderStepConfig {
+public class SkipExceptionStepConfig {
     @Autowired
     PlatformTransactionManager transactionManager;
 
     @Bean
-    public Step jdbcCursorReaderStep(@Qualifier("jdbcPagingReader") ItemReader<Cliente> jdbcCursorReader,
-                                     ItemWriter<Cliente> jdbcCursorWriter, JobRepository jobRepository) {
-        return new StepBuilder("jdbcCursorReaderStep", jobRepository)
-                .<Cliente, Cliente>chunk(1, transactionManager)
-                .reader(jdbcCursorReader)
-                .writer(jdbcCursorWriter)
+    public Step skipExceptionStep(ItemReader<Cliente> skipExceptionReader, ItemWriter<Cliente> skipExceptionWriter, JobRepository jobRepository) {
+        return new StepBuilder("skipExceptionStep", jobRepository)
+                .<Cliente, Cliente>chunk(11, transactionManager)
+                .reader(skipExceptionReader)
+                .writer(skipExceptionWriter)
+                .faultTolerant()
+                .skip(Exception.class)
+                .skipLimit(2)
                 .build();
-
     }
 }
